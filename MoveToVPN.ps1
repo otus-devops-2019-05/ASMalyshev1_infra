f4c0827878460dd058a43e186acf612ec74bf50c
Aleksey Malyshev <mail@asmalyshev.ru>
2019-06-25 22:31:25 +0300

    GCP
    cloud-testapp

---
+++ MoveToVPN.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+Set-Location $PSScriptRoot
+
+$VpnFolder = 'VPN'
+
+IF(!(Test-Path -Path .\$VpnFolder)){
+New-Item -Path .\ -Name $VpnFolder -ItemType Directory -Force
+}
+
+Get-ChildItem -Path .\|Where-Object {$_.Extension -eq '.ovpn' -or $_.name -eq 'setupvpn.sh'}|Move-Item -Destination .\$VpnFolder -Force
