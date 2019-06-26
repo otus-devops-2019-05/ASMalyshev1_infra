dfdbf70f22d4a5def3b2d9b74b6ac728c624f3f5
ASMalyshev <mail@asmalyshev.ru>
2019-06-26 10:10:34 +0300

    Update gcloud.ps1

--- gcloud.ps1
+++ gcloud.ps1
@@ -1,5 +1,5 @@
 ﻿Clear-Host
 
 [array]$IW = Invoke-WebRequest -Uri https://gist.githubusercontent.com/Nklya/5bc429c6ca9adce1f7898e7228788fe5/raw/01f9e4a1bf00b4c8a37ca6046e3e4d4721a3316a/gcloud -Method Get
-
+ 
 (-split $IW.Content) -replace "\\" -join " "
